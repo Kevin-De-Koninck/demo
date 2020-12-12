7258948cfd43be2ea5b5027dc43058872fd9245f
Kevin De Koninck <kevin.de.koninck92@gmail.com>
2020-12-12 13:38:51 +0100

    Update actions

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,31 +1,22 @@
-import logging
 import pytest
 
 
-LOGGER = logging.getLogger(__name__)
+@pytest.fixture
+def app():
+    app = main.create_app()
+    app.debug = True
+    return app.test_client()
 
 
-def test_app(capsys, demo_object):
-    # pylint: disable=W0612,W0613
-    LOGGER.info("Running the method 'hello_world' and checking the output on stdout.")
-    demo_object.hello_world()
-    captured = capsys.readouterr()
-    assert "Hello World" in captured.out
+def test_hello_world(app):
+    res = app.get("/")
+    # print(dir(res), res.status_code)
+    assert res.status_code == 200
+    assert b"Hello World" in res.data
 
 
-def test_inc(demo_object):
-    # pylint: disable=W0612,W0613
-    LOGGER.info("Initializing Demo class and running the method 'inc'")
-    demo_object.inc()
-    assert demo_object.value == 2
-
-
-def test_systemerror(capsys, demo_object):
-    with pytest.raises(SystemExit) as pytest_wrapped_e:
-        demo_object.raise_systemerror()
-    assert pytest_wrapped_e.type == SystemExit
-    assert pytest_wrapped_e.value.code == 1
-    captured = capsys.readouterr()
-    assert "Logging info is not captured in pytest." not in captured.out.split("\n")
-    assert "Standard print output is captured in pytest." in captured.out.split("\n")
+def test_some_id(app):
+    res = app.get("/foo/12345")
+    assert res.status_code == 200
+    assert b"12345" in res.data
 
